cd110f1e5b7294c5ad0506f1698fad9334b3199b
saimir.ismaili <saimir.ismaili@students.uni-mannheim.de>
2025-11-02 23:35:46 +0100

    feat: method to receive URL and frequency from user and trigger pinger for a single website

--- website_monitoring_app/app.py
+++ website_monitoring_app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from pinger import ping_websites
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+#Currently printing the responses from a single website
+@app.route('/status', methods=['POST'])
+def status():
+    temp_website_dict = dict()
+    temp_website_dict["url"] = request.form.get('url')
+    temp_website_dict["freq"] = int(request.form.get('frequency'))
+    ping_websites([temp_website_dict])
 
 if __name__ == '__main__':
     app.run(debug=True)
