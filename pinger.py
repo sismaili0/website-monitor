d01b9875c02eba4a791e7712df640f66b1729458
saimir.ismaili <saimir.ismaili@students.uni-mannheim.de>
2025-11-03 19:17:22 +0100

    feat: continuosly ping saved websites and log their availability together with relevant data

--- pinger.py
+++ pinger.py
@@ -1,26 +1,36 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-import requests
+import requests, sqlite3, datetime, time
+from db_tools import *
 
 #Send an GET request to a given URL
 def ping(url):
     try:
         response = requests.get(url)
-        print(f"{url} - Status: {response.status_code}")
+        response_time = response.elapsed.total_seconds()
+        log_availability(url, response.status_code, response_time)
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print(url, current_time)
     except Exception as e:
         print(f"Error {url}: {e}")
 
-#Repeated pinging for given websites with specific intervals
-def ping_websites(websites_list):
-    scheduler = BackgroundScheduler()
-    print(websites_list)
-    for website in websites_list:
-        scheduler.add_job(ping, 'interval', seconds=website['freq'], args=[website['url']])
+#Repeated pinging for given websites with specific frequency
+def ping_websites():
+    websites = get_all_websites_from_db()
+    next_pings = {w["url"]: time.time() for w in websites}
 
-    scheduler.start()
+    while True:
+        now = time.time()
 
-    try:
-        import time
-        while True:
-            time.sleep(1)
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
+        # Refresh list every 10 seconds
+        if int(now) % 10 == 0:
+            websites = get_all_websites_from_db()
+            for w in websites:
+                if w["url"] not in next_pings:
+                    next_pings[w["url"]] = now
+
+        for w in websites:
+            if now >= next_pings[w["url"]]:
+                ping(w["url"])
+                next_pings[w["url"]] = now + w["frequency"]
+
+        time.sleep(0.5)
