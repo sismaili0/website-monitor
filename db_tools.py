ccbc0404d697b256be3909a043e18d61e8ed1c91
sismaili0 <118765702+sismaili0@users.noreply.github.com>
2025-11-04 20:37:14 +0100

    Enhance UI to display monitored websites and logs
    * feat: add CSS file for styling
    
    * feat: enhance HTML to list currently monitored websites
    
    * feat: add HTML file displaying availability, avg response time, current status, and logs for selected website
    
    * feat: update app.py to handle endpoints and data transfer for website monitoring
    
    * feat: add methods to fetch data for display
    
    * chore: rename method to reflect changes

--- db_tools.py
+++ db_tools.py
@@ -1,12 +1,14 @@
-import sqlite3, datetime
+import sqlite3
+from datetime import datetime, timezone
 
 def initialize_db():
-    conn = sqlite3.connect('website_monitor.db')
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
     cursor = conn.cursor()
 
     cursor.execute('''
             CREATE TABLE IF NOT EXISTS websites (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT UNIQUE NOT NULL,
                 url TEXT UNIQUE NOT NULL,
                 frequency INTEGER NOT NULL DEFAULT 60
             )
@@ -27,19 +29,19 @@ def initialize_db():
     conn.close()
 
 #TODO Check later what happends if same url is entered again (prob. error)
-def add_website(url, frequency):
-    conn = sqlite3.connect('website_monitor.db')
+def add_website(name, url, frequency):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
     cursor = conn.cursor()
     cursor.execute(
-        f"INSERT INTO websites (url, frequency) VALUES (?, ?)", 
-        (url, frequency)
+        f"INSERT INTO websites (name, url, frequency) VALUES (?, ?, ?)", 
+        (name, url, frequency)
     )
     conn.commit()
     conn.close()
 
 #Log the ping with website_id, response status, time and timestamp
 def log_availability(url, status, response_time):
-    conn = sqlite3.connect('website_monitor.db')
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
     cursor = conn.cursor()
      # Get website_id from websites table
     cursor.execute("SELECT id FROM websites WHERE url = ?", (url,))
@@ -49,7 +51,7 @@ def log_availability(url, status, response_time):
         return
 
     website_id = row[0]
-    requested_at = datetime.datetime.utcnow().isoformat()
+    requested_at = datetime.now(timezone.utc).isoformat()
 
     # Insert into ping_logs
     cursor.execute(
@@ -63,14 +65,265 @@ def log_availability(url, status, response_time):
     conn.commit()
     conn.close()
 
-# Helps refresh the database when user enters new url
-def get_all_websites_from_db():
-    conn = sqlite3.connect('website_monitor.db')
+# Helps refresh the data when new entrys
+def get_user_entered_data():
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+
     cursor = conn.cursor()
-    cursor.execute("SELECT url, frequency FROM websites")
+    cursor.execute("SELECT name, url, frequency FROM websites")
     rows = cursor.fetchall()
+    
+    websites = [{"name": row[0], "url": row[1], "frequency": row[2], "current_status" : get_website_current_status(row[1])} for row in rows]
+    
     conn.close()
-    # Convert each row to a dictionary
-    websites = [{"url": row[0], "frequency": row[1]} for row in rows]
     return websites
 
+#Status gets updated every time the homepage is reloaded or a new user data is entered (WebSocket could solve real time update)
+def get_website_current_status(url):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id FROM websites WHERE url = ?", (url,))
+    row = cursor.fetchone()
+    if row is None:
+        return "no data"
+
+    website_id = row[0]
+
+    cursor.execute(
+        """
+        SELECT status FROM ping_logs 
+        WHERE website_id = ? 
+        ORDER BY requested_at DESC 
+        LIMIT 1
+        """,
+        (website_id,)
+    )
+    last_status_row = cursor.fetchone()
+
+    conn.close()
+
+    if last_status_row is None:
+        return "no data"
+
+    last_status = last_status_row[0]
+
+    if last_status == "200":
+        return "online"
+    else:
+        return "warning"
+
+#Delete the website and all its logs
+def delete_website(url):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+
+    # Find website id for the given URL
+    cursor.execute('SELECT id FROM websites WHERE url = ?', (url,))
+    result = cursor.fetchone()
+
+    if result:
+        website_id = result[0]
+        cursor.execute('DELETE FROM ping_logs WHERE website_id = ?', (website_id,))
+        cursor.execute('DELETE FROM websites WHERE id = ?', (website_id,))
+        conn.commit()
+    
+    conn.close()
+
+#Get all logs of the given URL
+def get_log_of_website(url):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM websites WHERE url = ?", (url,))
+    row = cursor.fetchone()
+    if not row:
+        logs = []
+    else:
+        website_id = row[0]
+        # Fetch ping logs for the website
+        cursor.execute("SELECT status, response_time, requested_at FROM ping_logs WHERE website_id = ?", (website_id,))
+        logs = cursor.fetchall()
+    conn.close()
+
+    return logs
+
+#Calculate website availability (available = 2xx Status Code), avarage response time, total checks and current status
+import sqlite3
+from datetime import datetime, timezone
+
+def initialize_db():
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS websites (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT UNIQUE NOT NULL,
+                url TEXT UNIQUE NOT NULL,
+                frequency INTEGER NOT NULL DEFAULT 60
+            )
+    ''')
+
+        # Create ping_logs table for storing results
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS ping_logs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                website_id INTEGER NOT NULL,
+                status TEXT,
+                response_time REAL,
+                requested_at TEXT,
+                FOREIGN KEY (website_id) REFERENCES websites (id)
+            )
+    ''')
+    conn.commit()
+    conn.close()
+
+#TODO Check later what happends if same url is entered again (prob. error)
+def add_website(name, url, frequency):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+    cursor.execute(
+        f"INSERT INTO websites (name, url, frequency) VALUES (?, ?, ?)", 
+        (name, url, frequency)
+    )
+    conn.commit()
+    conn.close()
+
+#Log the ping with website_id, response status, time and timestamp
+def log_availability(url, status, response_time):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+     # Get website_id from websites table
+    cursor.execute("SELECT id FROM websites WHERE url = ?", (url,))
+    row = cursor.fetchone()
+    if row is None:
+        conn.close()
+        return
+
+    website_id = row[0]
+    requested_at = datetime.now(timezone.utc).isoformat()
+
+    # Insert into ping_logs
+    cursor.execute(
+        """
+        INSERT INTO ping_logs (website_id, status, response_time, requested_at) 
+        VALUES (?, ?, ?, ?)
+        """,
+        (website_id, status, response_time, requested_at)
+    )
+
+    conn.commit()
+    conn.close()
+
+# Helps refresh the data when new entrys
+def get_user_entered_data():
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, url, frequency FROM websites")
+    rows = cursor.fetchall()
+    
+    websites = [{"name": row[0], "url": row[1], "frequency": row[2], "current_status" : get_website_current_status(row[1])} for row in rows]
+    
+    conn.close()
+    return websites
+
+#Status gets updated every time the homepage is reloaded or a new user data is entered (WebSocket could solve real time update)
+def get_website_current_status(url):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id FROM websites WHERE url = ?", (url,))
+    row = cursor.fetchone()
+    if row is None:
+        return "no data"
+
+    website_id = row[0]
+
+    cursor.execute(
+        """
+        SELECT status FROM ping_logs 
+        WHERE website_id = ? 
+        ORDER BY requested_at DESC 
+        LIMIT 1
+        """,
+        (website_id,)
+    )
+    last_status_row = cursor.fetchone()
+
+    conn.close()
+
+    if last_status_row is None:
+        return "no data"
+
+    last_status = last_status_row[0]
+
+    if last_status == "200":
+        return "online"
+    else:
+        return "warning"
+
+#Delete the website and all its logs
+def delete_website(url):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+
+    # Find website id for the given URL
+    cursor.execute('SELECT id FROM websites WHERE url = ?', (url,))
+    result = cursor.fetchone()
+
+    if result:
+        website_id = result[0]
+        cursor.execute('DELETE FROM ping_logs WHERE website_id = ?', (website_id,))
+        cursor.execute('DELETE FROM websites WHERE id = ?', (website_id,))
+        conn.commit()
+    
+    conn.close()
+
+#Get all logs of the given URL
+def get_log_of_website(url):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM websites WHERE url = ?", (url,))
+    row = cursor.fetchone()
+    if not row:
+        logs = []
+    else:
+        website_id = row[0]
+        # Fetch ping logs for the website
+        cursor.execute("SELECT status, response_time, requested_at FROM ping_logs WHERE website_id = ?", (website_id,))
+        logs = cursor.fetchall()
+    conn.close()
+
+    return logs
+
+#Calculate website availability (available = 2xx Status Code), avarage response time, total checks and current status
+def get_website_availability_stats(url):
+    conn = sqlite3.connect('website_monitor.db', timeout=5)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name FROM websites WHERE url = ?", (url,))
+    row = cursor.fetchone()
+    if not row:
+        logs = []
+        success_ratio = 0
+        average_response_time = 0
+        website_name = None
+    else:
+        website_id, website_name = row
+        cursor.execute("SELECT status, response_time, requested_at FROM ping_logs WHERE website_id = ?", (website_id,))
+        logs = cursor.fetchall()
+
+        success_count = sum(1 for log in logs if str(log[0]).startswith('2'))
+        total_count = len(logs)
+        success_ratio = str(round((success_count / total_count) * 100, 4)) + "%" if total_count > 0 else 0
+
+        if total_count > 0:
+            total_response_time = sum(log[1] for log in logs if log[1] is not None)
+            average_response_time = round(total_response_time / total_count, 4)
+        else:
+            average_response_time = 0
+
+    conn.close()
+    current_status = get_website_current_status(url)
+
+    return success_ratio, website_name, average_response_time, current_status
--- website_monitoring_app/app.py
+++ website_monitoring_app/app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sys, os, threading
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from pinger import ping_websites, add_website
-from db_tools import initialize_db
+from db_tools import *
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        add_website(request.form['url'], request.form['frequency'])
-    return render_template('home.html')
+        try:
+            add_website(request.form.get('website_name'), request.form.get('url'), request.form.get('frequency'))
+        except sqlite3.IntegrityError:
+            print("Warning, you might have entered the URL of a currently under monitor website")
+        return redirect(url_for('home'))
+    
+    user_entered_data = get_user_entered_data()
+    return render_template('home.html', urls = user_entered_data)
+
+@app.route('/status', methods=['GET', 'POST'])
+def status_of_website():
+    if request.method == 'POST':
+        selected_url = request.form.get('website_url')
+    else:
+        selected_url = request.args.get('website_url')
+    log_of_website = get_log_of_website(selected_url)
+    website_availability, website_name, average_response_time, current_status = get_website_availability_stats(selected_url)
+    return render_template('status.html', url = selected_url, log_of_website = log_of_website, 
+                           website_availability = website_availability, website_name=website_name,
+                             average_response_time = average_response_time, current_status = current_status )
+
+@app.route('/delete', methods=['POST'])
+def delete_monitored_website():
+    url = request.form.get('website_url')
+    delete_website(url)
+    return redirect(url_for('home')) 
+
 
 if __name__ == '__main__':
     initialize_db()
     threading.Thread(target=ping_websites, daemon=True).start()
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=False, use_reloader=False)
